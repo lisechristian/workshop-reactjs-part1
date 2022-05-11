60436386a59b2dba89977930c48f6dd7bc834ea1
Lise CHRISTIAN <lise-emmanuelle.christian@epitech.eu>
2022-05-11 16:53:25 +0200

    [ADD] exercises 1, 2, 3

--- exercises/01.4-building-a-layout/app.jsx
+++ exercises/01.4-building-a-layout/app.jsx
@@ -12,7 +12,20 @@ const data = {
 	},
 };
 
-let content = <img src={data.image} />;
+let content = (
+	<div class="card m-5">
+		<img class="card-img-top" src={data.image} alt="Card image cap" />
+		<div class="card-body">
+			<h5 class="card-title">{data.cardTitle}</h5>
+			<p class="card-text">{data.cardDescription}</p>
+			<a href={data.button} class="btn btn-primary">
+				Go somewhere
+			</a>
+		</div>
+	</div>
+);
+
+//let content = <img src={data.image} />;
 
 /**
  * define the variable 'content' here and fill it with the
